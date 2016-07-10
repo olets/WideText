b6b8afd137549902f303c5fcb86bdb8edd682446
Henry Bley-Vroman <bley.vroman@gmail.com>
2016-07-10 14:14:27 -0400

    working on v3 syntax

--- widetext.js
+++ widetext.js
@@ -6,13 +6,16 @@
    Copyright (c) 2016 Henry Bley-Vroman (@olets)
    MIT License
 
+
+working towards 3.1, 
  */
 
 function wideText(args) {
   var defaults = {
     'selector': '.widetext',
     'leading': .2,
-    'align': ''
+    'align': '',
+    'row': 'tspan'
   }
 
   if (typeof args === "string")
@@ -21,74 +24,103 @@ function wideText(args) {
     }
   if (typeof args !== "object")
     args = {};
-  for (var i in defaults)
-    if (!(i in args))
-      args[i] = defaults[i];
+  for (var option in defaults)
+    if (!(option in args))
+      args[option] = defaults[option];
 
-  // Find all the targets of WideText
+    // Find all the targets of WideText
   var wtElems = document.querySelectorAll(args.selector);
-  
-  for (i = 0; i < wtElems.length; ++i) {
+
+  for (var i = 0; i < wtElems.length; ++i) {
     var wtElem = wtElems[i];
+
+    // Add the WideText wrappers
+    //
+    // Build the `text` element
+    var textElem = document.createElement("text");
+    // Wrap the target's contents in it
+    while (wtElem.firstChild)// because there could be one or several children
+      textElem.appendChild(wtElem.firstChild);
+    wtElem.appendChild(textElem);
+    textElem.classList.add("wideText_text");
+    //
+    // Build the `svg` element
+    var svgElem = document.createElement("svg");
+    // wrap the `text` in it
+    svgElem.appendChild(textElem);
+    wtElem.appendChild(svgElem);
+    svgElem.classList.add("wideText_svg");
+    svgElem.setAttribute('width', '100%');
+    svgElem.setAttribute('display', 'block');
+    svgElem.setAttribute('font-size', '6px');
     
-    // Add the WideText markup
-    var wrapped = '<svg class="widetext_svg"><text class="widetext_text">' + wtElem.innerHTML + '</text></svg>';
-    wtElem.innerHTML = wrapped;
+    // console.log('wtElem:');
+    // console.log(wtElem);
+    // console.log('svgElem:');
+    // console.log(svgElem);
+    
 
-    // Find all the `svg.widetext`s
-    var svgElems = document.querySelectorAll(args.selector + ' svg');
+    // if the target has data-align, apply that to the `text`
+    var align = wtElem.getAttribute('data-align') || args.align;
+    if (align == 'center') {
+      var anchor = 'middle'
+    } else if (align == 'right')
+      var anchor = 'end';
+    if (anchor) textElem.setAttribute('text-anchor', anchor);
 
-    // For each `svg.widetext`
-    for (n = 0; n < svgElems.length; ++n) {
-      var svgElem = svgElems[i];
-
-      // set the `svg`s display styles
-      svgElem.setAttribute('width', '100%');
-      svgElem.setAttribute('display', 'block');
-      svgElem.setAttribute('font-size', '6px');
-
-      // get the child `text`
-      var textElem = svgElem.querySelector('text');
-
-      // if the `svg` has data-align, apply that to the `text`
-      var align = wtElem.getAttribute('data-align') || args.align;
-      if (align == 'center') {
-        var anchor = 'middle'
-      } else if (align == 'right')
-        var anchor = 'end';
-      if (anchor) textElem.setAttribute('text-anchor', anchor);
-
-      // Put all `tspan`s on their own line and apply leading
-      //
-      //
-      // Get all child `tspan`s
-      var tspanElems = textElem.querySelectorAll('tspan'),
-        n;
-
-      // For each of them
-      for (var m = 0; m < tspanElems.length; ++m) {
-        var tspanElem = tspanElems[m];
-
-        // set `x` to the left
-        tspanElem.setAttribute('x', '0');
-
-        // if the `svg.svgtext` has `data-leading`, use that as the leading;
-        // otherwise default to a .2em leading
-        // console.log('args.leading:' + args.leading);
-        var leadingVal = wtElem.getAttribute('data-leading') || args.leading;
-        var dyVal = parseFloat(leadingVal, 10) + 1 + "em";
-        tspanElem.setAttribute('dy', dyVal);
+    if (args.row !== 'tspan') {
+      var rowElems = textElem.querySelectorAll(args.row);
+      for (p = 0; p < rowElems.length; p++) {
+        var rowElem = rowElems[p];
+        var tspanElem = document.createElement("tspan");
+        tspanElem.appendChild(rowElem.firstChild);
+        textElem.replaceChild(tspanElem, rowElem);
       }
-
-      // size the svg to fit the text
-      //
-      // This solution is adapted, with many thanks, from Paul LeBeau
-      //
-      // get the `svg.svgtext text`s bounding box
-      var textBBox = textElem.getBBox();
-      // and update the `svg.svgtext`s viewBox to fit it to the text
-      svgElem.setAttribute("viewBox", [textBBox.x, textBBox.y, textBBox.width, textBBox.height].join(' '));
+      console.log(textElem)
     }
 
+    // Put all `tspan`s on their own line and apply leading
+    //
+    //
+    // Get all child `tspan`s
+    var tspanElems = textElem.querySelectorAll('tspan'),
+      m;
+
+    // For each of them
+    for (m = 0; m < tspanElems.length; ++m) {
+      var tspanElem = tspanElems[m];
+
+      // set `x` to the left
+      tspanElem.setAttribute('x', '0');
+
+      // if the `svg.svgtext` has `data-leading`, use that as the leading;
+      // otherwise default to a .2em leading
+      // console.log('args.leading:' + args.leading);
+      var leadingVal = wtElem.getAttribute('data-leading') || args.leading;
+      var dyVal = parseFloat(leadingVal, 10) + 1 + "em";
+      tspanElem.setAttribute('dy', dyVal);
+    }
+    
+    // size the svg to fit the text
+    //
+    // get the `svg.svgtext text`s bounding box
+    // var textBBox = textElem.getBBox();
+    // and update the `svg.svgtext`s viewBox to fit it to the text
+    // svgElem.setAttribute("viewBox", [textBBox.x, textBBox.y, textBBox.width, textBBox.height].join(' '));
+    // console.log('textElem:');
+    // console.log(textElem);
+    // console.log('textBBox:');
+    // console.log(textBBox);
+    //
+    // This doesn't work - seems the DOM isn't recognizing the svg as an svg?
+    // maybe it has to be loaded, a la https://www.raymondcamden.com/2013/02/05/Playing-with-SVG-and-JavaScript/ ?
   }
 }
+
+wideText();
+wideText({
+  selector: '.widetext_parent > section',
+  leading: 3,
+  align: 'right',
+  row: 'span'
+})
